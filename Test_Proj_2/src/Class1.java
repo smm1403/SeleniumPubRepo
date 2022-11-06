13dd8197532c318ff20961eaa5193d20d01f49de
baps <baps@DESKTOP-0OB75F3>
2022-11-06 13:57:02 +0530

    commit2

--- Test_Proj_2/src/Class1.java
+++ Test_Proj_2/src/Class1.java
@@ -1,9 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Class1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver","C:\\Users\\baps\\Downloads\\chromedriver_win32\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
 
-	}
+
+Actions ac=new Actions(driver);
+
+driver.get("https://jqueryui.com/droppable/");
+
+driver.manage().window().maximize(); //Maximize the window
+
+
+driver.switchTo().frame(driver.findElement(By.className("demo-frame"))); //switching to iframe
+
+WebElement src=driver.findElement(By.id("draggable"));
+
+WebElement des=driver.findElement(By.id("droppable"));
+
+ac.dragAndDrop(src, des).perform();
+
+}
 
 }
